e67cf52183098724a351abe3c20fbd456226732b
rick45024 <nikshehtra@gmail.com>
2025-12-17 14:22:51 +0530

    bbg

---
+++ bbg.js
@@ -0,0 +1,346 @@
+<script type='text/javascript'>
+//<![CDATA[
+// Blogger Auto Internal Linking Script
+(function() {
+    'use strict';
+    
+    // Configuration - Edit these keywords and URLs for your blog
+    const linkConfig = {
+        // Format: 'keyword': 'your-post-url'
+'Sunny Leone': '/2015/04/paani-wala-dance-sunny-leone.html',
+'Yo Yo Honey Singh': '/2015/03/honey-singh-one-bottle-down.html',
+'OST': '/2015/03/dil-ka-kya-rung-karun-ost.html',
+'OST': '/2015/03/aye-zindagi-ost-qb-drama.html',
+'OST': '/2015/03/dil-e-barbad-ost-song.html',
+'OST': '/2015/03/tum-se-mil-kay-ost-song.html',
+'Arijit Singh': '/2015/03/janib-arijit-singh-songs.html',
+'Sunny Leone': '/2015/03/khuda-bhi-leela-sunny.html',
+'Atif Aslam': '/2015/03/jeena-tere-bina-badlapur-atif.html',
+'Miss Pooja': '/2015/03/tipsy-hogai-miss-pooja.html',
+'Arijit Singh': '/2015/03/tere-bin-nhi-laage-ek-paheli.html',
+'Maninder Buttar': '/2015/03/kite-kalli-maninder-buttar-song.html',
+'Kamal Khaira': '/2015/03/selfian-kamal-khaira-preet.html',
+'Badshah': '/2015/03/2-many-girls-badshah.html',
+'Bohemia': '/2015/03/koi-ni-parwah-haji-bohemia.html',
+'Kulwinder Billa': '/2015/03/chakkwe-suit-kulwinder-billa.html',
+'Ranjit Bawa': '/2015/03/yaari-chandigarh-ranjit-bawa.html',
+'Sunny Leone': '/2015/02/desi-look-sunny-leone-ek-paheli-leela.html',
+'Ammy Virk': '/2015/01/taara-ammy-virk-music-video.html',
+'Arijit Singh': '/2014/12/tu-chale-i-arijit-singh.html',
+'Arijit Singh': '/2014/12/sooraj-dooba-hai-roy-arijit.html',
+'Arijit Singh': '/2014/12/khamoshiyan-song-arijit.html',
+'Gippy Grewal': '/2014/12/party-like-punjabi-gippy-grewal.html',
+'Hardy Sandhu': '/2014/10/joker-hardy-sandhu-circus.html',
+'Kulwinder Billa': '/2014/10/tayari-haan-di-kulwinder-billa.html',
+'Jazzy B': '/2014/10/mitran-de-boot-jazzy-b-kaurb.html',
+'Jassi Gill': '/2014/10/bapu-zimidar-jassi-gill-song.html',
+'Arijit Singh': '/2014/10/o-soniye-titoo-mba-arijit-singh.html',
+'Honey Singh': '/2014/10/alcoholic-hoon-honey-singh-akshay.html',
+'Diljit Dosanjh': '/2014/09/this-singh-is-so-stylish-diljit.html',
+'Prabh Gill': '/2014/09/pyaar-tere-da-prabh-gill.html',
+'Bilal Saeed': '/2014/09/lethal-combination-bilal-saeed.html',
+'Arijit Singh': '/2014/08/sawan-aaya-hai-arijit-singh-song.html',
+'Raftaar': '/2014/08/happy-single-song-raftaar-big-dhillon.html',
+'Honey Singh': '/2014/08/love-dose-honey-singh-song.html',
+'Honey Singh': '/2014/08/desi-kalakaar-honey-singh-song.html',
+'Arijit Singh': '/2014/06/tenu-samjhawan-arijit-humty-sharma.html',
+'Mustafa Zahid': '/2014/05/zaroorat-mustafa-zahid-ek-villain.html',
+'Arijit Singh': '/2014/05/blame-the-night-holiday-arijit-singh-mp3.html',
+'Jassi Gill': '/2014/04/tamanna-meri-jassi-gill-dilvilpyarvyaar.html',
+'Diljit Dosanjh': '/2014/04/selfie-diljit-dosanjh-mp3-song.html',
+'Honey Singh': '/2014/04/ice-cream-khaungi-honey-singh.html',
+'Arijit Singh': '/2014/02/suno-na-sangemarmar-arijit-singh.html',
+'Honey Singh': '/2014/02/chaar-bottle-vodka-honeysingh.html',
+'Arijit Singh': '/2014/01/main-dhoondne-ko-heartless-arijit.html',
+'Raftaar': '/2014/01/gucci-armani-raftaar-song.html',
+'Ammy Virk': '/2014/01/adhoore-chaa-ammy-virk-mp3.html',
+'Diljit Dosanjh': '/2014/01/diljit-soorma-song-mp3.html',
+'Farhan Saeed': '/2013/12/halka-halka-suroor-farhan-saeed.html',
+'Falak Shabir': '/2013/12/judah-falak-shabir-mp3-download.html',
+'Yo Yo Honey Singh': '/2024/11/millionaire-yo-yo-honey-singh.html',
+'Anuv Jain': '/2024/11/mere-husn-ke-ilawa-husn-anuv-jain.html',
+'Ishq': '/2024/11/main-aa-likhoon-tu-aa-jaaye-lyrics-ishq.html',
+'Anuv Jain': '/2024/11/jo-tum-mere-ho-lyrics-anuv-jain.html',
+'Ammy Virk': '/2024/11/gall-mann-le-meri-ammy-virk.html',
+'Kaifi Khalil': '/2023/10/kaifi-khalil-kahani-suno-lyrics.html',
+'Arijit Singh': '/2023/10/pasoori-nu-arijit-singh.html',
+'Tony Kakkar': '/2021/06/dil-tera-jadoo-tutega-ta-pata-laguga.html',
+'Harnoor': '/2021/06/pehli-chan-vekhya-harnoor.html',
+'Sharry Maan': '/2021/06/dilwale-lyrics-sharry-maan.html',
+'Sidhu Moose Wala': '/2021/06/me-my-girlfriend-sidhu.html',
+'Sidhu Moose Wala': '/2021/06/sidhu-son-sidhu-moose-wala.html',
+'Raja Kumari': '/2021/06/us-lyrics-sidhu-raja-kumarii.html',
+'Sidhu Moose Wala': '/2021/06/sidhu-moose-walabrown-shortie.html',
+'Sidhu Moose Wala': '/2021/06/moosedrilla-sidhu-moose-divine.html',
+'Kaka': '/2021/05/kale-rang-libaas-kaka.html',
+'Kaka': '/2021/05/temporary-pyar-kaka-darling-umra-da.html',
+'Hardy Sandhu': '/2019/01/tu-na-jaane-hardy-sandhu.html',
+'Gajendra Verma': '/2019/01/khelegi-kya-gajendra-verma.html',
+'Gippy Grewal': '/2017/12/nai-shad-da-gippy-grewal.html',
+'Nimrat Khaira': '/2017/12/designer-nimrat-khaira-new-song.html',
+'Prabh Gill': '/2017/12/tareyan-de-des-prabh-gill.html',
+'Diljit Dosanjh': '/2017/12/raat-di-gedi-diljit-dosanjh-neeru-bajwa.html',
+'Yo Yo Honey Singh': '/2017/12/dil-chori-sada-ho-honey-singh.html',
+'Ed Sheeran': '/2017/01/ed-sheeran-castle-on-hill.html',
+'Ed Sheeran': '/2017/01/shape-of-you-ed-sheeran.html',
+'Diljit Dosanjh': '/2017/01/do-you-know-diljit-song.html',
+'Amrinder Gill': '/2017/01/ni-mainu-amrinder-gill-sarvann.html',
+'Kulwinder Billa': '/2017/01/antenna-kulwinder-billa.html',
+'Prabh Gill': '/2017/01/bacha-prabh-gill-disha.html',
+'Arijit Singh': '/2017/01/channa-mereya-arijit-singh.html',
+'Arijit Singh': '/2016/03/bolna-mahi-arijit-singh.html',
+'Arijit Singh': '/2015/12/gerua-dilwale-arijit-singh.html',
+'Atif Aslam': '/2017/01/pehli-dafa-atif-aslam.html',
+'Hardy Sandhu': '/2017/01/backbone-hardy-sandhu.html',
+'Raftaar': '/2016/06/instagram-love-raftaar.html',
+'Guru Randhawa': '/2016/06/suit-guru-randhawa-arjun.html',
+'Amrit Maan': '/2016/05/camaro-kaali-amrit-maan.html',
+'Zack Knight': '/2016/05/enemy-lyrics-zack-knights-new-song-2016.html',
+'Yo Yo Honey Singh': '/2016/04/high-heels-ki-ka-honey-singh.html',
+'Ankit Tiwari': '/2016/02/sanam-teri-kasam-lyrics-ankit-tiwari.html',
+'Nooran Sisters': '/2015/12/jindriye-nooran-sisters.html',
+'Badshah': '/2015/09/wakhra-swag-badshah.html',
+'Kanika Kapoor': '/2015/09/teddy-bear-kanika-ikka.html',
+'Jazzy B': '/2014/10/mitran-de-boot-jazzy-b-kaurb.html',
+'Jassi Gill': '/2014/10/bapu-zimidar-jassi-gill-song.html',
+'Arijit Singh': '/2014/10/o-soniye-titoo-mba-arijit-singh.html',
+'Bilal Khan': '/2014/10/lagne-laga-bilal-khan.html',
+'Omar Nadeem': '/2014/10/girti-deewar-omar-nadeem-song.html',
+'Yo Yo Honey Singh': '/2014/10/alcoholic-hoon-honey-singh-akshay.html',
+'Babbu Maan': '/2014/10/babbu-maan-khat-mera-gham-2.html',
+'Ali Zafar': '/2014/09/kill-dil-title-song-ali-zafar.html',
+'Diljit Dosanjh': '/2014/09/this-singh-is-so-stylish-diljit.html',
+'Prabh Gill': '/2014/09/pyaar-tere-da-prabh-gill.html',
+'Arijit Singh': '/2014/09/meherbaan-bang-bang-movie.html',
+'Bilal Saeed': '/2014/09/lethal-combination-bilal-saeed.html',
+'Mehtab Virk': '/2014/08/haar-jaani-aa-mehtab-virk-song.html',
+'Aarsh Benipal': '/2014/08/rang-sanwla-aarsh-benipal-song.html',
+'Yo Yo Honey Singh': '/2014/08/love-dose-honey-singh-song.html',
+'Yo Yo Honey Singh': '/2014/08/desi-kalakaar-honey-singh-song.html',
+'Arijit Singh': '/2014/08/sawan-aaya-hai-arijit-singh-song.html',
+'Raftaar': '/2014/08/happy-single-song-raftaar-big-dhillon.html',
+'Falak Shabir': '/2014/08/teri-kasam-falak-shabir-song.html',
+'Bohemia': '/2014/08/akkha-mumbai-bohemia-ft-mun-e-fame.html',
+'Farhan Saeed': '/2014/08/naam-e-wafa-creature-3d-farhan-saeed.html',
+'Gippy Grewal': '/2014/08/26-ban-gayi-gippy-grewal-jazzyb.html',
+'Badshah': '/2014/08/bandook-badshah-raxstar-song-rap.html',
+'Shrey Singhal': '/2014/07/shrey-singhal-koi-fariyaad-song.html',
+'Farhan Saeed': '/2014/07/farhan-saeed-roiyaan-song-royi-aan.html',
+'Hamza Malik': '/2014/07/sajnaa-hamza-malik-song-2014.html',
+'Badshah': '/2014/06/chull-badshah-rap-fazilpuria-song.html',
+'Arijit Singh': '/2014/06/tenu-samjhawan-arijit-humty-sharma.html',
+'Diljit Dosanjh': '/2014/06/teri-pag-channo-diljit-punjab-1984.html',
+'Prabh Gill': '/2014/06/ve-naina-prabh-gill-oh-my-pyo-ji.html',
+'Arijit Singh': '/2014/06/aaj-phir-tumpe-hate-story-2-arijit.html',
+'Arijit Singh': '/2014/06/galiyan-ek-villain-song.html',
+'Diljit Dosanjh': '/2014/06/rangroot-diljit-punjab-1984-movie.html',
+'Mustafa Zahid': '/2014/05/zaroorat-mustafa-zahid-ek-villain.html',
+'Soch Band': '/2014/05/awari-soch-band-ek-villain.html',
+'Bohemia': '/2014/05/bohemia-swag-mp3-download-song.html',
+'Jassi Gill': '/2014/05/suraj-song-jassi-gill-roshan-prince.html',
+'Bilal Saeed': '/2014/05/bewafa-bilal-saeed-mp3-irfan-nazar.html',
+'Arijit Singh': '/2014/05/blame-the-night-holiday-arijit-singh-mp3.html',
+'Jassi Gill': '/2014/04/tamanna-meri-jassi-gill-dilvilpyarvyaar.html',
+'Diljit Dosanjh': '/2014/04/sweetu-diljit-disco-singh-song.html',
+'Mohit Chauhan': '/2014/04/rabba-heropanti-song-mohit-chauhan.html',
+'Diljit Dosanjh': '/2014/04/selfie-diljit-dosanjh-mp3-song.html',
+'Arjun': '/2014/04/kabhi-jo-badal-arjun-mp3.html',
+'Ranjit Bawa': '/2014/04/sadi-vaari-aun-de-ranjit-bawa.html',
+'Diljit Dosanjh': '/2014/04/happy-birthday-diljit-disco-singh.html',
+'Babbu Maan': '/2014/04/21st-century-saadh-babbu-maan.html',
+'Yo Yo Honey Singh': '/2014/04/ice-cream-khaungi-honey-singh.html',
+'Arijit Singh': '/2014/03/tum-hi-ho-arjun-you-got-bad.html',
+'Ammy Virk': '/2014/03/bullet-chammak-challo-ammy-virk.html',
+'Diljit Dosanjh': '/2014/03/beautiful-billo-diljit-disco-singh.html',
+'Yo Yo Honey Singh': '/2014/03/honey-singh-party-with-bhoothnath.html',
+'Offo': '/2014/03/offo-song-2-states-music.html',
+'Kulbir Jhinjer': '/2014/03/rakhwan-kota-song-kulbir-jhinjer-mp3.html',
+'Yuvraaj Hans': '/2014/03/tere-layi-yuvraaj-hans-mr-mrs-420.html',
+'Besharmi Ki Height': '/2014/03/sari-raat-besharmi-ki-height.html',
+'Main Tera Hero': '/2014/03/tera-dhyan-kidhar-hai-main-tera-hero.html',
+'Jassi Gill': '/2014/03/naina-nu-jassi-gill-mr-mrs-420.html',
+'Diljit Dosanjh': '/2014/03/rayban-diljit-dosanjh-song-mp3.html',
+'Badshah': '/2014/03/shanivaar-raati-rap-main-tera-hero.html',
+'Main Tera Hero': '/2014/03/galat-baat-hai-main-tera-hero.html',
+'Yo Yo Honey Singh': '/2014/02/mast-kalander-honey-singh-song-mika.html',
+'Falak Shabir': '/2014/02/falak-bholi-bhali-pyaari.html',
+'Ali Zafar': '/2014/02/nahi-maloom-ali-total-siyapaa.html',
+'Shael': '/2014/02/jaan-ve-shael-song-mp3.html',
+'Shakira': '/2014/02/shakira-cant-remember-forget-you.html',
+'Farhan Akhtar': '/2014/02/ahista-ahista-song-farhan-akhtar.html',
+'Sunny Leone': '/2014/02/baby-doll-ragini-mms2-sunny-leone.html',
+'Shael': '/2014/02/shael-soniye-hiriye-teri-yaad.html',
+'Shael': '/2014/02/shael-zindagi-mp3-download.html',
+'Ram Leela': '/2014/02/ram-chahe-leela-song-ramleela.html',
+'Shaadi Ke Side Effects': '/2014/02/yahan-vahaan-wahan-shaad-side-effects.html',
+'Shaadi Ke Side Effects': '/2014/02/romance-ho-zara-shaadi-ke-side-effects.html',
+'Arijit Singh': '/2014/02/suno-na-sangemarmar-arijit-singh.html',
+'Babbal Rai': '/2014/02/akh-teri-babbal-rai-mp3download.html',
+'Maninder Buttar': '/2014/02/yaari-maninder-buttar-mp3.html',
+'Shrey Singhal': '/2014/02/hamqadam-shrey-singhal-mp3.html',
+'Gajendra Verma': '/2014/02/saajna-re-song-gajendra-verma.html',
+'Yo Yo Honey Singh': '/2014/02/chaar-bottle-vodka-honeysingh.html',
+'Queen Movie': '/2014/01/kinare-queen-movie-songs.html',
+'Shaadi Ke Side Effects': '/2014/01/tumse-pyar-ho-gaya-shaadi-side-effects.html',
+'Dhoom 3': '/2014/01/kamli-dhoom3-mp3-song.html',
+'Heartless': '/2014/01/ishq-khuda-heartless-movie.html',
+'Highway': '/2014/01/kahaan-hoon-main-highway-movie.html',
+'Money Aujla': '/2014/01/gori-london-ayi-money-aujla.html',
+'Amanat Ali': '/2014/01/naina-lagay-amanat-ali-mp3.html',
+'Amrinder Gill': '/2014/01/mera-deewanapan-amrinder-gill.html',
+'Jazzy B': '/2014/01/harry-not-brahmachari-jazzy-b.html',
+'KK': '/2014/01/soniye-heartless-songs-kk.html',
+'Arijit Singh': '/2014/01/main-dhoondne-ko-heartless-arijit.html',
+'Gunday': '/2014/01/tune-mari-entriyaan-gunday.html',
+'Raftaar': '/2014/01/gucci-armani-raftaar-song.html',
+'Ammy Virk': '/2014/01/adhoore-chaa-ammy-virk-mp3.html',
+'Bilal Saeed': '/2014/01/choothi-bilal-saeed-waqar-mp3.html',
+'Diljit Dosanjh': '/2014/01/diljit-soorma-song-mp3.html',
+'Gunday': '/2014/01/saaiyaan-gunday-songs.html',
+'A R Rahman': '/2014/01/pataka-guddi-ar-rahman.html',
+'Queen Movie': '/2014/01/london-thumakda-queen-movie.html',
+'Hasee Toh Phasee': '/2014/01/manchala-hasee-toh-phasee.html',
+'Hasee Toh Phasee': '/2014/01/zehnaseeb-hasee-toh-phasee.html',
+'KK': '/2014/01/tere-bin-ho-na-sakega-gujara-kk.html',
+'Kaur B': '/2014/01/feeling-kaur-b-song-mp3.html',
+'Heartless': '/2014/01/mashooqana-heartless-movie-songs.html',
+'Ikka': '/2014/01/bad-girl-sherlyn-chopra-ikka.html',
+'Farhan Saeed': '/2013/12/halka-halka-suroor-farhan-saeed.html',
+'Gunday': '/2013/12/jashn-e-ishqa-gunday-movie.html',
+'Hussnain Lahori': '/2013/12/harjaee-hussnain-lahori-song.html',
+'Yaariyan': '/2013/12/allah-waariyan-yaariyan-movie.html',
+'Falak Shabir': '/2013/12/judah-falak-shabir-mp3-download.html',
+'Jai Ho': '/2013/12/tumko-toh-ana-hi-tha-jai-ho.html',
+'Jai Ho': '/2013/12/tere-naina-jai-ho-movie.html',
+'Yo Yo Honey Singh': '/2013/12/horn-ok-please-honey-singh-Ishqiya.html',
+'Hasee Toh Phasee': '/2013/12/hasee-toh-phasee-drama-queen.html',
+'Karle Pyaar Karle': '/2013/12/tanhaai-karle-pyaar-karle-movie.html',
+'Jai Ho': '/2013/12/baaki-sab-first-class-jai-ho.html',
+'Ishq Bector': '/2013/12/mere-siva-ishq-bector.html'
+    };
+
+    // Settings
+    const settings = {
+        maxLinksPerKeyword: 2,
+        caseSensitive: false,
+        targetBlank: false,
+        addNoFollow: false,
+        excludeTags: ['A', 'SCRIPT', 'STYLE', 'CODE', 'PRE'],
+        contentSelector: '.post-body, .entry-content, .post-content'
+    };
+
+    // Function to escape regex special characters
+    function escapeRegex(string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    }
+
+    // Function to create internal links
+    function createInternalLinks() {
+        const contentArea = document.querySelector(settings.contentSelector);
+        if (!contentArea) return;
+
+        // Track how many times each keyword has been linked
+        const linkCounts = {};
+        Object.keys(linkConfig).forEach(keyword => {
+            linkCounts[keyword] = 0;
+        });
+
+        // Function to process text nodes
+        function processNode(node) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                let text = node.textContent;
+                let parent = node.parentNode;
+                
+                // Skip if parent is an excluded tag
+                if (settings.excludeTags.includes(parent.tagName)) {
+                    return;
+                }
+
+                // Skip if already inside a link
+                if (parent.closest('a')) {
+                    return;
+                }
+
+                let modified = false;
+                
+                // Process each keyword
+                Object.entries(linkConfig).forEach(([keyword, url]) => {
+                    if (linkCounts[keyword] >= settings.maxLinksPerKeyword) {
+                        return;
+                    }
+
+                    const flags = settings.caseSensitive ? 'g' : 'gi';
+                    const regex = new RegExp('\\b' + escapeRegex(keyword) + '\\b', flags);
+                    
+                    if (regex.test(text)) {
+                        text = text.replace(regex, function(match) {
+                            if (linkCounts[keyword] < settings.maxLinksPerKeyword) {
+                                linkCounts[keyword]++;
+                                
+                                let linkAttributes = `href="${url}"`;
+                                if (settings.targetBlank) {
+                                    linkAttributes += ' target="_blank"';
+                                }
+                                if (settings.addNoFollow) {
+                                    linkAttributes += ' rel="nofollow"';
+                                }
+                                
+                                return `<a ${linkAttributes} class="auto-internal-link">${match}</a>`;
+                            }
+                            return match;
+                        });
+                        modified = true;
+                    }
+                });
+
+                // Replace the text node with new HTML if modified
+                if (modified) {
+                    const wrapper = document.createElement('div');
+                    wrapper.innerHTML = text;
+                    
+                    // Replace the text node with the new nodes
+                    while (wrapper.firstChild) {
+                        parent.insertBefore(wrapper.firstChild, node);
+                    }
+                    parent.removeChild(node);
+                }
+            } else if (node.nodeType === Node.ELEMENT_NODE) {
+                // Recursively process child nodes
+                const childNodes = Array.from(node.childNodes);
+                childNodes.forEach(child => processNode(child));
+            }
+        }
+
+        // Start processing from the content area
+        processNode(contentArea);
+    }
+
+    // Function to run when DOM is ready
+    function init() {
+        if (document.readyState === 'loading') {
+            document.addEventListener('DOMContentLoaded', createInternalLinks);
+        } else {
+            createInternalLinks();
+        }
+    }
+
+    // Initialize the script
+    init();
+
+    // Optional: Add CSS for auto-generated links
+    const style = document.createElement('style');
+    style.textContent = `
+        .auto-internal-link {
+            color: #0066cc;
+            text-decoration: underline;
+            transition: color 0.3s ease;
+        }
+        .auto-internal-link:hover {
+            color: #004499;
+        }
+    `;
+    document.head.appendChild(style);
+
+})();
+//]]>
+</script>
